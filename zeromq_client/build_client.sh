f01d5cda6d25ea69cabd99dce9828df64eb45298
ksyoo <ksyoo@localhost.localdomain>
2020-04-06 14:08:46 +0900

    add sample code of ZeroMQ server-client

---
+++ zeromq_client/build_client.sh
@@ -0,0 +1,5 @@
+g++ -o ./zeromq_client \
+  ./src/main.cpp -I /home/ksyoo/ksyooF/third_party/zeromq-4.1.7/include \
+  -L /home/ksyoo/ksyooF/third_party/zeromq-4.1.7/lib \
+  -l zmq \
+  -Wl,-rpath,/home/ksyoo/ksyooF/third_party/zeromq-4.1.7/lib
